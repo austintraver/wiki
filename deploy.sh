1d62bae1bb037e3b17bdcde234291374439e2a8e
Austin Traver <austintraver@users.noreply.github.com>
2020-10-21 05:52:24 -0700

    Add firebase deployment configuration

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 typeset filename=${${(%):-%N}:A:t}
+typeset site='https://helpful.wiki'
 
 if ((${#} != 1)) {
 	print "${filename}: invalid # of commands" >&2
@@ -10,7 +11,8 @@ if ((${#} != 1)) {
 if [[ ${1} == 'before' ]] {
 	hugo
 } elif [[ ${1} == 'after' ]] {
-	firebase open 'hosting:site'
+#	firebase open 'hosting:site'
+	open ${site}
 } else {
 	print -- "${filename}: unrecognized argument ${1}" >&2
 	exit 1
