a93747499bf806d877105a5064e936e5bd5d9b28
Austin Traver <austintraver@users.noreply.github.com>
2020-11-30 07:18:22 -0800

    Update the introduction on the front page

--- static/js/init.js
+++ static/js/init.js
@@ -1,2 +1,10 @@
 firebase.analytics()
 firebase.performance()
+
+document.addEventListener('DOMContentLoaded', function() {
+  /* Trim any leading or trailing whitespace from
+   the text content of any HTML sample elements */
+  for (let node of document.querySelectorAll('samp')) {
+    node.textContent = node.textContent.trim()
+  }
+})
--- static/js/single.js
+++
@@ -1,5 +0,0 @@
-/* Trim any leading or trailing whitespace from
-the text content of any HTML sample elements */
-for (let node of document.querySelectorAll('samp')) {
-  node.textContent = node.textContent.trim()
-}
