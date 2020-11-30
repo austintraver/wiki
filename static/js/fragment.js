22c0063c1a7874a87a3162c6ba4be411fba2b773
Austin Traver <austintraver@users.noreply.github.com>
2020-11-30 06:26:00 -0800

    Update styling and layouts

---
+++ static/js/fragment.js
@@ -0,0 +1,64 @@
+let toast = document.querySelector('div#toast')
+
+/* Display a toast to the user */
+const displayToast = async (content) => {
+  /* If the window is already displaying a toast,
+   don't try to display a new one */
+  if (toast.timeout) {
+    return
+  }
+  /* Display the toast for 3 seconds */
+  toast.textContent = content
+  toast.classList.add('show')
+  toast.timeout = setTimeout(clearToast, 3000)
+}
+/* Clear the current toast being displayed */
+const clearToast = async () => {
+  toast.classList.remove('show')
+  displayToast.textContent = ''
+  toast.timeout = null
+}
+const createLinks = async () => {
+  /* When the user clicks on the link, update the browser's URL to
+   include this header's ID in the hash */
+  const headings = document.querySelectorAll('h1,h2,h3,h4,h5,h6')
+  headings.forEach((heading) => {
+    let fragment = document.createElement('img')
+    fragment.classList.add('fragment')
+    fragment.src = '/img/link.svg'
+    fragment.height = 24
+    fragment.width = 24
+    fragment.addEventListener('click', async () => {
+      window.location.hash = `#${heading.id}`
+      if (window.isSecureContext) {
+        try {
+          await navigator.clipboard.writeText(`${heading.baseURI}#${heading.id}`)
+          await displayToast('Copied to clipboard')
+        } catch (error) {
+          alert(error)
+        }
+      }
+    })
+    heading.appendChild(fragment)
+    /* When the user hovers over the link icon,
+     make the icon appear fully opaque
+     */
+    fragment.addEventListener('mouseenter', () => {
+      fragment.style['opacity'] = '100%'
+    })
+    /* When the user hovers over the header of a particular section,
+     reduce the transparency of the link icon
+     */
+    heading.addEventListener('mouseover', () => {
+      fragment.style['opacity'] = '50%'
+    })
+    /* When the user is not hovering over the header
+     of any particular section, make the link icon transparent.
+     */
+    heading.addEventListener('mouseout', () => {
+      fragment.style['opacity'] = '20%'
+    })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', createLinks)
--- static/js/init.js
+++ static/js/init.js
@@ -1,18 +1,2 @@
-import * as
-
-// TODO: Replace the following with your app's Firebase project configuration
-var firebaseConfig = {
-  // ...
-};
-var firebaseConfig = {
-  apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
-  authDomain: "helpfulwiki.firebaseapp.com",
-  projectId: "myapp-project-123",
-  storageBucket: "myapp-project-123.appspot.com",
-  messagingSenderId: "65211879809",
-  appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
-  measurementId: "G-8GSGZQ44ST"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.analytics()
+firebase.performance()
