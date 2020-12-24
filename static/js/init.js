fed170281678b9e2cdc223dedf9e801c690db4c2
Austin Traver <austintraver@users.noreply.github.com>
2020-12-24 03:02:04 -0800

    Migrate layout to doctober

--- static/js/init.js
+++ static/js/init.js
@@ -1,10 +1,6 @@
-firebase.analytics()
-firebase.performance()
-
-document.addEventListener('DOMContentLoaded', function() {
-  /* Trim any leading or trailing whitespace from
-   the text content of any HTML sample elements */
-  for (let node of document.querySelectorAll('samp')) {
-    node.textContent = node.textContent.trim()
-  }
-})
+try {
+  firebase.analytics()
+  firebase.performance()
+} catch (error) {
+  console.error(error)
+}
